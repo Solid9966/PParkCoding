f1be41a162cd3ccb2a9dc05a16197be65f6df519
Sung Won Park <153745270+Solid9966@users.noreply.github.com>
2026-04-03 16:36:18 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 19.40 ms, Memory: 56.1 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,37 +1,39 @@
 import java.util.*;
 
 class Solution {
-    
     static boolean[][] visited;
     static int[][] dist;
+    
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
     
-    static class Point{
+    static class Point {
         int x,y;
-        public Point(int x, int y){
+         public Point (int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-    static int bfs(int sx,int sy,int M, int N,int[][]maps) {
+    
+    static void bfs (int sx, int sy, int n, int m, int[][] maps) {
         Queue<Point> q = new ArrayDeque<>();
-        visited[sy][sx] = true;
         q.add(new Point(sx,sy));
+        visited[sy][sx] = true;
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             Point p = q.poll();
-            int x = p.x; int y = p.y;
+            int x = p.x;
+            int y = p.y;
             
-            for(int i = 0; i< 4; i++){
+            for (int i = 0; i<4; i++) {
                 int nx = dx[i] + x;
                 int ny = dy[i] + y;
                 
-                // 맵 바깥,
-                if(nx >= M || nx < 0 || ny >= N || ny < 0) continue;
-                // 왔던길,
+                // 맵 바깐
+                if(nx >= n || nx < 0 || ny >= m || ny < 0) continue;
+                // 왔던 길
                 if(visited[ny][nx] == true) continue;
-                // 막힌길
+                // 벽
                 if(maps[ny][nx] == 0) continue;
                 
                 dist[ny][nx] = dist[y][x] + 1;
@@ -39,20 +41,23 @@ class Solution {
                 q.add(new Point(nx,ny));
             }
         }
-        return dist[N-1][M-1]+1;
     }
     
     public int solution(int[][] maps) {
-        int M = maps[0].length; // x
-        int N = maps.length; // y
         int answer = 0;
+        int n = maps[0].length; // x
+        int m = maps.length; // y
         
-        visited = new boolean[N][M];
-        dist = new int[N][M];
+        visited = new boolean[m][n];
+        dist = new int[m][n];
         
-        answer = bfs(0,0,M,N,maps);
-        if (answer == 1) answer = -1;
+        bfs(0,0,n,m,maps);
+        System.out.println(Arrays.deepToString(dist));
+        System.out.println(Arrays.deepToString(visited));
         
+        if (dist[m-1][n-1] == 0) answer = -1;
+        else answer = dist[m-1][n-1] + 1;
+            
         return answer;
     }
 }
